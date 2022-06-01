3859abd82790cdd1a6afc41011e6094d61753140
rtappa <104068471+rtappa@users.noreply.github.com>
2022-06-01 13:12:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_pytest_example.handlers.routes import configure_routes
+from handlers.routes import configure_routes
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
